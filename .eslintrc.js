5c676b7bdeb7d25162086decd5a2575db53b24fe
orangecoding <weakmap@gmail.com>
2021-10-07 11:19:30 +0200

    adding forgotten files

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    eqeqeq: [2, 'allow-null'],
+
+    // ###########################################################
+    // ### Semantics / Performance impacting
+    // ###########################################################
+    // babel inserts `'use strict';` for us
+    strict: 0,
+
+    'no-redeclare': [2, { builtinGlobals: false }],
+
+    // If a class method does not use this, it can safely be made a static function.
+    // http://eslint.org/docs/rules/class-methods-use-this
+    'class-methods-use-this': ['off'],
+
+    // ###########################################################
+    // ### Style
+    // ###########################################################
+    indent: ['off', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+};
